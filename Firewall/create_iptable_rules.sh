00a419e505510a50c44c8e555cdb671fb1f2f214
Jose Luis <jlrp09102005@gmail.com>
2026-01-23 11:43:00 +0100

    rules to accept loopback and full duplex established and related

--- Firewall/create_iptable_rules.sh
+++ Firewall/create_iptable_rules.sh
@@ -3,7 +3,7 @@
 tables=(filter nat mangle raw security)
 
 #====== Clean Rules ======
-for table in ${tables[@]}; do
+for table in "${tables[@]}"; do
     sudo iptables -t $table -F
     sudo iptables -t $table -X
     sudo iptables -t $table -Z
@@ -22,7 +22,7 @@ ip6tables -P INPUT -j DROP
 ip6tables -P OUTPUT -j DROP
 ip6tables -P FORWARD -j DROP
 
-iptables -A -i lo -j ACCEPT #Grant local port access
-iptables -A INPUT -m conntrack --cstate ESTABLISHED,RELATED -j ACCEPT
+iptables -A INPUT -i lo -j ACCEPT #Grant local port access
+iptables -A INPUT -m conntrack ESTABLISHED, RELATED -j ACCEPT
 
 #====== SSH ======
