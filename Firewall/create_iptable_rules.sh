57f619e674117d38d9bf2de6239d502b4f9f188d
Jose Luis <jlrp09102005@gmail.com>
2026-01-23 11:43:47 +0100

    Added double quotes for security

--- Firewall/create_iptable_rules.sh
+++ Firewall/create_iptable_rules.sh
@@ -4,13 +4,13 @@ tables=(filter nat mangle raw security)
 
 #====== Clean Rules ======
 for table in "${tables[@]}"; do
-    sudo iptables -t $table -F
-    sudo iptables -t $table -X
-    sudo iptables -t $table -Z
+    sudo iptables -t "$table" -F
+    sudo iptables -t "$table" -X
+    sudo iptables -t "$table" -Z
 
-    sudo ip6tables -t $table -F
-    sudo ip6tables -t $table -X
-    sudo ip6tables -t $table -Z
+    sudo ip6tables -t "$table" -F
+    sudo ip6tables -t "$table" -X
+    sudo ip6tables -t "$table" -Z
 done
 
 #====== Default Rules ======
