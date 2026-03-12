a363162ef9ee52b9857d9056b11220970e874948
Jose Luis <jlrp09102005@gmail.com>
2026-03-13 00:14:42 +0100

    fix test

--- Firewall/service-utils.sh
+++ Firewall/service-utils.sh
@@ -74,8 +74,11 @@ create_service_wizzard()
 
 configure_unit_wizzard()
 {
-    local -n arr="$1"
+    local -n _ref_unit="$1"
     local user_response
+
+    echo "DEBUG: arr referencia a -> $1"
+    echo "DEBUG: contenido actual -> " "${_ref_unit[@]}"
 
     print_info "To write more than 1 option: AddRequirement=req1,req2,req1" 1
     echo ""
@@ -85,10 +88,10 @@ configure_unit_wizzard()
     if check_yes_no_response "$user_response"; then
 
         read -r -p "Add Description [ENTER to skip]: " user_response
-        [[ -n "$user_response" ]] && arr+=("Description=$user_response")
+        [[ -n "$user_response" ]] && _ref_unit+=("Description=$user_response")
 
         read -r -p "Add Documentation [ENTER to skip]: " user_response
-        [[ -n "$user_response" ]] && arr+=("Documentation=$user_response")
+        [[ -n "$user_response" ]] && _ref_unit+=("Documentation=$user_response")
 
     fi
 
@@ -97,34 +100,34 @@ configure_unit_wizzard()
     if check_yes_no_response "$user_response"; then
 
         read -r -p "Add Requirement [ENTER to skip]: " user_response
-        [[ -n "$user_response" ]] && arr+=("Requires=$user_response")
+        [[ -n "$user_response" ]] && _ref_unit+=("Requires=$user_response")
 
         read -r -p "Add Overridable Requirement [ENTER to skip]: " user_response
-        [[ -n "$user_response" ]] && arr+=("RequiresOverridable=$user_response")
+        [[ -n "$user_response" ]] && _ref_unit+=("RequiresOverridable=$user_response")
 
         read -r -p "Add Requisite [ENTER to skip]: " user_response
-        [[ -n "$user_response" ]] && arr+=("Requisite=$user_response")
+        [[ -n "$user_response" ]] && _ref_unit+=("Requisite=$user_response")
 
         read -r -p "Add Requisite Overridable [ENTER to skip]: " user_response
-        [[ -n "$user_response" ]] && arr+=("RequisiteOverridable=$user_response")
+        [[ -n "$user_response" ]] && _ref_unit+=("RequisiteOverridable=$user_response")
 
         read -r -p "Add Wants [ENTER to skip]: " user_response
-        [[ -n "$user_response" ]] && arr+=("Wants=$user_response")
+        [[ -n "$user_response" ]] && _ref_unit+=("Wants=$user_response")
 
         read -r -p "Add Binds To [ENTER to skip]: " user_response
-        [[ -n "$user_response" ]] && arr+=("BindsTo=$user_response")
+        [[ -n "$user_response" ]] && _ref_unit+=("BindsTo=$user_response")
 
         read -r -p "Add Part Of [ENTER to skip]: " user_response
-        [[ -n "$user_response" ]] && arr+=("PartOf=$user_response")
+        [[ -n "$user_response" ]] && _ref_unit+=("PartOf=$user_response")
 
         read -r -p "Add Requires Mounts For [ENTER to skip]: " user_response
-        [[ -n "$user_response" ]] && arr+=("RequiresMountsFor=$user_response")
+        [[ -n "$user_response" ]] && _ref_unit+=("RequiresMountsFor=$user_response")
 
         read -r -p "Add Joins Namespace Of [ENTER to skip]: " user_response
-        [[ -n "$user_response" ]] && arr+=("JoinsNamespaceOf=$user_response")
+        [[ -n "$user_response" ]] && _ref_unit+=("JoinsNamespaceOf=$user_response")
 
         read -r -p "Add Conflicts [ENTER to skip]: " user_response
-        [[ -n "$user_response" ]] && arr+=("Conflicts=$user_response")
+        [[ -n "$user_response" ]] && _ref_unit+=("Conflicts=$user_response")
 
     fi
 
@@ -132,16 +135,16 @@ configure_unit_wizzard()
     if check_yes_no_response "$user_response"; then
 
         read -r -p "Add Before [ENTER to skip]: " user_response
-        [[ -n "$user_response" ]] && arr+=("Before=$user_response")
+        [[ -n "$user_response" ]] && _ref_unit+=("Before=$user_response")
 
         read -r -p "Add After [ENTER to skip]: " user_response
-        [[ -n "$user_response" ]] && arr+=("After=$user_response")
+        [[ -n "$user_response" ]] && _ref_unit+=("After=$user_response")
 
         read -r -p "Add On Failure [ENTER to skip]: " user_response
-        [[ -n "$user_response" ]] && arr+=("OnFailure=$user_response")
+        [[ -n "$user_response" ]] && _ref_unit+=("OnFailure=$user_response")
 
         read -r -p "Add On Failure Job Mode [ENTER to skip]: " user_response
-        [[ -n "$user_response" ]] && arr+=("OnFailureJobMode=$user_response")
+        [[ -n "$user_response" ]] && _ref_unit+=("OnFailureJobMode=$user_response")
 
     fi
 
@@ -149,10 +152,10 @@ configure_unit_wizzard()
     if check_yes_no_response "$user_response"; then
 
         read -r -p "Add Propagates Reload To [ENTER to skip]: " user_response
-        [[ -n "$user_response" ]] && arr+=("PropagatesReloadTo=$user_response")
+        [[ -n "$user_response" ]] && _ref_unit+=("PropagatesReloadTo=$user_response")
 
         read -r -p "Add Reload Propagated From [ENTER to skip]: " user_response
-        [[ -n "$user_response" ]] && arr+=("ReloadPropagatedFrom=$user_response")
+        [[ -n "$user_response" ]] && _ref_unit+=("ReloadPropagatedFrom=$user_response")
 
     fi
 
@@ -160,28 +163,28 @@ configure_unit_wizzard()
     if check_yes_no_response "$user_response"; then
 
         read -r -p "Add Condition Path Exists [ENTER to skip]: " user_response
-        [[ -n "$user_response" ]] && arr+=("ConditionPathExists=$user_response")
+        [[ -n "$user_response" ]] && _ref_unit+=("ConditionPathExists=$user_response")
 
         read -r -p "Add Condition Path Exists Glob [ENTER to skip]: " user_response
-        [[ -n "$user_response" ]] && arr+=("ConditionPathExistsGlob=$user_response")
+        [[ -n "$user_response" ]] && _ref_unit+=("ConditionPathExistsGlob=$user_response")
 
         read -r -p "Add Condition Path Is Directory [ENTER to skip]: " user_response
-        [[ -n "$user_response" ]] && arr+=("ConditionPathIsDirectory=$user_response")
+        [[ -n "$user_response" ]] && _ref_unit+=("ConditionPathIsDirectory=$user_response")
 
         read -r -p "Add Condition Path Is Symbolic Link [ENTER to skip]: " user_response
-        [[ -n "$user_response" ]] && arr+=("ConditionPathIsSymbolicLink=$user_response")
+        [[ -n "$user_response" ]] && _ref_unit+=("ConditionPathIsSymbolicLink=$user_response")
 
         read -r -p "Add Condition Path Is Mount Point [ENTER to skip]: " user_response
-        [[ -n "$user_response" ]] && arr+=("ConditionPathIsMountPoint=$user_response")
+        [[ -n "$user_response" ]] && _ref_unit+=("ConditionPathIsMountPoint=$user_response")
 
         read -r -p "Add Condition Directory Not Empty [ENTER to skip]: " user_response
-        [[ -n "$user_response" ]] && arr+=("ConditionDirectoryNotEmpty=$user_response")
+        [[ -n "$user_response" ]] && _ref_unit+=("ConditionDirectoryNotEmpty=$user_response")
 
         read -r -p "Add Condition File Not Empty [ENTER to skip]: " user_response
-        [[ -n "$user_response" ]] && arr+=("ConditionFileNotEmpty=$user_response")
+        [[ -n "$user_response" ]] && _ref_unit+=("ConditionFileNotEmpty=$user_response")
 
         read -r -p "Add Condition File Is Executable [ENTER to skip]: " user_response
-        [[ -n "$user_response" ]] && arr+=("ConditionFileIsExecutable=$user_response")
+        [[ -n "$user_response" ]] && _ref_unit+=("ConditionFileIsExecutable=$user_response")
 
     fi
 
@@ -189,28 +192,28 @@ configure_unit_wizzard()
     if check_yes_no_response "$user_response"; then
 
         read -r -p "Add Condition Architecture [ENTER to skip]: " user_response
-        [[ -n "$user_response" ]] && arr+=("ConditionArchitecture=$user_response")
+        [[ -n "$user_response" ]] && _ref_unit+=("ConditionArchitecture=$user_response")
 
         read -r -p "Add Condition Virtualization [ENTER to skip]: " user_response
-        [[ -n "$user_response" ]] && arr+=("ConditionVirtualization=$user_response")
+        [[ -n "$user_response" ]] && _ref_unit+=("ConditionVirtualization=$user_response")
 
         read -r -p "Add Condition Security [ENTER to skip]: " user_response
-        [[ -n "$user_response" ]] && arr+=("ConditionSecurity=$user_response")
+        [[ -n "$user_response" ]] && _ref_unit+=("ConditionSecurity=$user_response")
 
         read -r -p "Add Condition Capability [ENTER to skip]: " user_response
-        [[ -n "$user_response" ]] && arr+=("ConditionCapability=$user_response")
+        [[ -n "$user_response" ]] && _ref_unit+=("ConditionCapability=$user_response")
 
         read -r -p "Add Condition Kernel Command Line [ENTER to skip]: " user_response
-        [[ -n "$user_response" ]] && arr+=("ConditionKernelCommandLine=$user_response")
+        [[ -n "$user_response" ]] && _ref_unit+=("ConditionKernelCommandLine=$user_response")
 
         read -r -p "Add Condition AC Power [ENTER to skip]: " user_response
-        [[ -n "$user_response" ]] && arr+=("ConditionACPower=$user_response")
+        [[ -n "$user_response" ]] && _ref_unit+=("ConditionACPower=$user_response")
 
         read -r -p "Add Condition Needs Update [ENTER to skip]: " user_response
-        [[ -n "$user_response" ]] && arr+=("ConditionNeedsUpdate=$user_response")
+        [[ -n "$user_response" ]] && _ref_unit+=("ConditionNeedsUpdate=$user_response")
 
         read -r -p "Add Condition Null [ENTER to skip]: " user_response
-        [[ -n "$user_response" ]] && arr+=("ConditionNull=$user_response")
+        [[ -n "$user_response" ]] && _ref_unit+=("ConditionNull=$user_response")
 
     fi
 
@@ -218,19 +221,19 @@ configure_unit_wizzard()
     if check_yes_no_response "$user_response"; then
 
         read -r -p "Add Condition User [ENTER to skip]: " user_response
-        [[ -n "$user_response" ]] && arr+=("ConditionUser=$user_response")
+        [[ -n "$user_response" ]] && _ref_unit+=("ConditionUser=$user_response")
 
         read -r -p "Add Condition Group [ENTER to skip]: " user_response
-        [[ -n "$user_response" ]] && arr+=("ConditionGroup=$user_response")
+        [[ -n "$user_response" ]] && _ref_unit+=("ConditionGroup=$user_response")
 
         read -r -p "Add Condition Environment [ENTER to skip]: " user_response
-        [[ -n "$user_response" ]] && arr+=("ConditionEnvironment=$user_response")
+        [[ -n "$user_response" ]] && _ref_unit+=("ConditionEnvironment=$user_response")
 
         read -r -p "Add Condition Form Field [ENTER to skip]: " user_response
-        [[ -n "$user_response" ]] && arr+=("ConditionFormField=$user_response")
+        [[ -n "$user_response" ]] && _ref_unit+=("ConditionFormField=$user_response")
 
         read -r -p "Add Condition Extension [ENTER to skip]: " user_response
-        [[ -n "$user_response" ]] && arr+=("ConditionExtension=$user_response")
+        [[ -n "$user_response" ]] && _ref_unit+=("ConditionExtension=$user_response")
 
     fi
 
@@ -238,16 +241,16 @@ configure_unit_wizzard()
     if check_yes_no_response "$user_response"; then
 
         read -r -p "Add Refuse Manual Start [ENTER to skip]: " user_response
-        [[ -n "$user_response" ]] && arr+=("RefuseManualStart=$user_response")
+        [[ -n "$user_response" ]] && _ref_unit+=("RefuseManualStart=$user_response")
 
         read -r -p "Add Refuse Manual Stop [ENTER to skip]: " user_response
-        [[ -n "$user_response" ]] && arr+=("RefuseManualStop=$user_response")
+        [[ -n "$user_response" ]] && _ref_unit+=("RefuseManualStop=$user_response")
 
         read -r -p "Add Refuse Manual Reload [ENTER to skip]: " user_response
-        [[ -n "$user_response" ]] && arr+=("RefuseManualReload=$user_response")
+        [[ -n "$user_response" ]] && _ref_unit+=("RefuseManualReload=$user_response")
 
         read -r -p "Allow Isolate [ENTER to skip]: " user_response
-        [[ -n "$user_response" ]] && arr+=("AllowIsolate=$user_response")
+        [[ -n "$user_response" ]] && _ref_unit+=("AllowIsolate=$user_response")
 
     fi
 
@@ -255,19 +258,19 @@ configure_unit_wizzard()
     if check_yes_no_response "$user_response"; then
 
         read -r -p "Add Default Dependencies [ENTER to skip]: " user_response
-        [[ -n "$user_response" ]] && arr+=("DefaultDependencies=$user_response")
+        [[ -n "$user_response" ]] && _ref_unit+=("DefaultDependencies=$user_response")
 
         read -r -p "Add Stop When Unneeded [ENTER to skip]: " user_response
-        [[ -n "$user_response" ]] && arr+=("StopWhenUnneeded=$user_response")
+        [[ -n "$user_response" ]] && _ref_unit+=("StopWhenUnneeded=$user_response")
 
         read -r -p "Add Ignore On Isolate [ENTER to skip]: " user_response
-        [[ -n "$user_response" ]] && arr+=("IgnoreOnIsolate=$user_response")
+        [[ -n "$user_response" ]] && _ref_unit+=("IgnoreOnIsolate=$user_response")
 
         read -r -p "Add Ignore On Snapshot [ENTER to skip]: " user_response
-        [[ -n "$user_response" ]] && arr+=("IgnoreOnSnapshot=$user_response")
+        [[ -n "$user_response" ]] && _ref_unit+=("IgnoreOnSnapshot=$user_response")
 
         read -r -p "Add CollectMode [ENTER to skip]: " user_response
-        [[ -n "$user_response" ]] && arr+=("CollectMode=$user_response")
+        [[ -n "$user_response" ]] && _ref_unit+=("CollectMode=$user_response")
 
     fi
 
@@ -275,13 +278,13 @@ configure_unit_wizzard()
     if check_yes_no_response "$user_response"; then
 
         read -r -p "Add Job Timeout Sec [ENTER to skip]: " user_response
-        [[ -n "$user_response" ]] && arr+=("JobTimeoutSec=$user_response")
+        [[ -n "$user_response" ]] && _ref_unit+=("JobTimeoutSec=$user_response")
 
         read -r -p "Add Job Timeout Action [ENTER to skip]: " user_response
-        [[ -n "$user_response" ]] && arr+=("JobTimeoutAction=$user_response")
+        [[ -n "$user_response" ]] && _ref_unit+=("JobTimeoutAction=$user_response")
 
         read -r -p "Add Job Timeout Reboot Arg [ENTER to skip]: " user_response
-        [[ -n "$user_response" ]] && arr+=("JobTimeoutRebootArg=$user_response")
+        [[ -n "$user_response" ]] && _ref_unit+=("JobTimeoutRebootArg=$user_response")
 
     fi
 
@@ -289,7 +292,7 @@ configure_unit_wizzard()
     if check_yes_no_response "$user_response"; then
 
         read -r -p "Add Source Path [ENTER to skip]: " user_response
-        [[ -n "$user_response" ]] && arr+=("SourcePath=$user_response")
+        [[ -n "$user_response" ]] && _ref_unit+=("SourcePath=$user_response")
 
     fi
 }
